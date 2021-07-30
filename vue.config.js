81e943d708bcc0e73b7251f418172a9c5f901a06
1621151730 <1621151730@qq.com>
2021-07-30 13:01:12 +0800

    第二次提交

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+//这里的这些配置会和公共的进行合拼的
+module.exports={
+ configureWebpack:{//配置webpack的config
+    resolve:{
+     // extensions:[] //这个不用配置了，系统默认配好了.js .vue .css
+      extensions:[],
+      alias:{
+        '@':'src',//这个是默认的别名@对应的就是src
+        'assets':'@/assets',
+        'common':'@/assets',
+        'acomponents':'@/assets',
+        'network':'@/assets',
+      //  'router':'@/router',  就引用了少量的不需要给他配
+        'views':'@/assets',
+      }
+    }
+ }
+}
